72918fed7d3c9add74554c7726d6e3a7328ef31a
Thiago Mendes <thiagopaulino89@gmail.com>
2021-05-05 19:48:11 -0300

    teste

---
+++ src/Aula1.kt
@@ -0,0 +1,29 @@
+fun main() {
+//    //criando variaveis e outras coisas introdutorias
+//val lista1 = listOf("Marilene", "Dona Cutilada", "Morre Diabo")
+//    // mesmo que a variavel seja mutavel (var) é necessario setar se a lista é variavel ou não (listof () ou mutablelistof()
+//    println(lista1.first())
+//
+//    // when substitui o if, pode ter quantas condições eu quiser e uma boa praticar é a ultima condição ser um else
+//var numero = 7
+//    when {
+//        numero < 3 -> println("Menor que 3")
+//        numero > 3 -> println("Maior que 3")
+//        else -> println("igual a 3")
+//    }
+//
+//    //for (variavel numero inicial .. numero final step de quanto em quanto ele pula
+//for (num in 1..10 step 1){
+//        println(num)
+//    }
+
+//    // varrer lista com o for - o it se refere ao item que está sendo verificado, no caso, lista1
+//    lista1.forEach {
+//        println(lista1)
+//    }
+    var x = 0
+    while (x < 2){
+        println(x)
+        x++
+    }
+}
